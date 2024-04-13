53a94b07c69aa74fcf45f23d20af9fe78808758b
Mahdi Ansari <mahdi.ansari1@ucalgary.ca>
2024-04-13 15:37:42 -0600

    Add border for footer active item

--- src/components/FooterNavbar.js
+++ src/components/FooterNavbar.js
@@ -3,14 +3,19 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faTicket, faQrcode, faCommentDots, faGifts } from '@fortawesome/free-solid-svg-icons';
 import ThemeContext from '../contexts/ThemeContext';
+import { useLocation } from 'react-router-dom';
 
 const FooterNavbar = () => {
-    const { theme } = useContext(ThemeContext);
+    const location = useLocation();
+    const isActive = (path) => location.pathname.startsWith(path);
+    const { theme } = useContext(ThemeContext);    
+    const activeLinkClass = `border-top border-3 ${theme === 'light' ? 'border-dark text-dark' : 'border-light text-light'}`;
+    const inactiveLinkClass = `border-top border-3 ${theme === 'dark' ? 'border-dark' : 'border-light'}`;
     const navbarClass = theme === 'dark' ? 'navbar-dark bg-dark' : 'navbar-light bg-light';
     const bgColor = theme === 'light' ? 'white' : '';
 
     return (
-        <Navbar bg={bgColor} fixed="bottom" className={`d-block d-md-none ${navbarClass}`}>
+        <Navbar bg={bgColor} fixed="bottom" className={`d-block d-md-none py-0 ${navbarClass}`}>
             <Container className="justify-content-around">
                 <Nav className="w-100 text-center">
                     {[
@@ -20,8 +25,8 @@ const FooterNavbar = () => {
                         { href: "/messaging", icon: faCommentDots, text: "Messaging" },
                         { href: "/gifts", icon: faGifts, text: "Gifts" }
                     ].map((link) => (
-                        <Nav.Link href={link.href} key={link.text} className="flex-grow-1">
-                            <FontAwesomeIcon icon={link.icon} className="fs-4" />
+                        <Nav.Link href={link.href} key={link.text} className={`flex-grow-1 ${isActive(link.href) ? activeLinkClass : inactiveLinkClass}`}>
+                            <FontAwesomeIcon icon={link.icon} className="fs-2" />
                             <div className="d-block" style={{ fontSize: '12px' }}>{link.text}</div>
                         </Nav.Link>
                     ))}
