ac8eaf9ee9c6f7816d31c12b1adf3fcae0a15dd3
Mahdi Ansari <mahdi.ansari1@ucalgary.ca>
2024-05-17 14:27:06 -0600

    Remove the tile

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ const News = () => {
   };
   return (
     <div className='mh-100 py-5 feature-row'>
-      <h1>News Page</h1>
+      <h1>&nbsp;</h1>
       <Media
         countryISOCode="ca"
         domain="1"
